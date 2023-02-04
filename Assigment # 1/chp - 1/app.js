177df84a9411d3dd5f0ab30519b4d9de266ddceb
Abdul Nafay <103064913+AbdulNafay42@users.noreply.github.com>
2023-02-04 21:48:56 +0500

    Add files via upload

---
+++ Assigment # 1/chp - 1/app.js
@@ -0,0 +1,21 @@
+//Question # 1
+
+ alert("hello sir")
+
+//Question # 2
+
+  alert("Error! Please enter a valid passward.")
+
+//Question # 3
+
+  alert("\n Welcome to js land \n Happy Coding!")
+
+//Question # 4
+
+  alert("\n Welcome to js land...")
+
+  alert("\n Happy Coding!")
+
+//Question # 5
+
+//console
